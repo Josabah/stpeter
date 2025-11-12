67ea2813dc120fd82af0dd91fd54c820b1a126ce
John Doe <tedibeshah@gmail.com>
2025-11-13 00:07:56 +0300

    update

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"IOjEWuefWfxfdvW7Z+y4HBarYisLNhPeBHBCAfkdc8E=\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"FP/GmPkhW9JiCLNOs4voAQg+Z2B+gAoJqpQAWLueZCQ=\"\n}"
